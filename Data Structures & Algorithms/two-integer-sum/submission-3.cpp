45bf576d48f5ce5e93e797d923ecca533dae258c
masonyooooooo <masonyooooooo@gmail.com>
2026-08-17 07:23:26 -0700

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+            if (seen.contains(need))
+                return {seen[need], i};
+            else
+                seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
