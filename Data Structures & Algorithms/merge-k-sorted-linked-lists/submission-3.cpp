75770e947df16bea8172e28ca27954a1ee65495b
masonyooooooo <masonyooooooo@gmail.com>
2026-08-18 08:18:17 -0700

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.cpp
@@ -0,0 +1,28 @@
+/**
+ * Iteration:
+ *      iterate each head of all list -> find the smallest one and insert
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* res = new ListNode(0);
+        ListNode* cur = res;
+
+        while (true) {
+            int minNode = -1;
+            for (int i = 0; i < lists.size(); i++) {
+                if (!lists[i]) continue;
+                if (minNode == -1 || lists[minNode]->val > lists[i]->val) {
+                    minNode = i;
+                }
+            }
+
+            if (minNosw == -1) break;
+            cur->next = lists[minNode];
+            lists[minNode] = lists[minNode]->next;
+            cur = cur->next;
+        }
+        return res->next;
+    }
+};
