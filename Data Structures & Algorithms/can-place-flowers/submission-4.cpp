6461255eab02622919490eaca1a26cbc1c2a67f6
masonyooooooo <masonyooooooo@gmail.com>
2026-08-20 20:37:22 -0700

    Add: can-place-flowers - submission-4

---
+++ Data Structures & Algorithms/can-place-flowers/submission-4.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int i = 0;
+
+        while (i < flowerbed.size() - 1) {
+            if (flowerbed[i + 1] == 1) {
+                // next pos is occupied
+                i += 3;
+            } else {
+                if (flowerbed[i] == 0) {
+                    n--;
+                    flowerbed[i] = 1;
+                }
+                i += 2;
+
+            }
+        }
+
+        if (i == flowerbed.size() - 1 && flowerbed.back() == 0)
+            n--;
+
+        if (n <= 0)
+            return true;
+
+        return false;
+    }
+};
