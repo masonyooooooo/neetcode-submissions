e27be452a393accca516e9649bf4663cac984b4b
masonyooooooo <masonyooooooo@gmail.com>
2026-08-20 19:45:13 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> counts;
+
+        for (char c : s) {
+            if (counts.find(c) == counts.end()) {
+                counts[c] = 1;
+            } else {
+                counts[c]++;
+            }
+        }
+
+        for (char c : t) {
+            if (counts.find(c) == counts.end()) {
+                return false;
+            } else {
+                counts[c]--;
+            }
+        }
+
+        for (auto& [c, count] : counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
