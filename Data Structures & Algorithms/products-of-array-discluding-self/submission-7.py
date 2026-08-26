cdf7e974acdc08f4cbee4bffef2c9f9f524932cf
masonyooooooo <masonyooooooo@gmail.com>
2026-08-25 21:39:25 -0700

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.py
@@ -0,0 +1,33 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        Space Optimization: 
+            two vars: one left product and one right product of nums[i]
+
+        Complexity:
+            Time: O(n)
+            Space: O(1)
+        """
+
+        res: list[int] = [0] * len(nums)
+        lprod = rprod = 1
+        zeros = 0
+
+        for i in range(len(nums) - 1, 1, -1):
+            if nums[i] == 0:
+                rprod = rprod * nums[i]
+            else:
+                zeros += 1
+
+        if zeros > 1:
+            return res
+
+        for i, num in enumerate(nums):
+            if zeros == 1:
+                res[i] = 0 if num != 0 else rprod * lprod
+                continue
+            res[i] = rprod * lprod
+            rprod /= num
+            lprod *= num
+
+        return res
