a236d19508d5fcc7e02f1f0194d38c370d7f2b11
masonyooooooo <masonyooooooo@gmail.com>
2026-08-27 22:48:45 -0700

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        """
+        [CAUTION] The array may contain negative numbers. So, it cannot be solved by Sliding Window, because there is no rules for moving boundaries.
+        Hash Map + Prefix Sum (like Two-Sum):
+            Hash Map: preSum : num of preSum
+            Assumpe: prefix sum of position `i` is `p`
+            diff = p - k
+            if diff == 0:
+                res += 1 -> [0, i] is the only subarray ended at i
+            if diff != 0: -> I still need to substract diff
+                res += preSum[diff] -> num of subarraies whose sum is diff
+        """
+
+        res = 0
+        curSum = 0
+        preSum = {0 : 1}
+
+        for num in nums:
+            curSum += num
+            diff = curSum - k
+
+            res += preSum.get(diff, 0)
+            preSum[curSum] = 1 + preSum.get(curSum, 0)
+
+        return res
